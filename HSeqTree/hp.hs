c2878750c9b595f729a49f28f69220ea4f9742d3
Kotaro-Hamu <69944611+Kotaro-Hamu@users.noreply.github.com>
2021-03-05 16:47:42 +0900

    Update hp.hs

--- HSeqTree/hp.hs
+++ HSeqTree/hp.hs
@@ -1,117 +1,57 @@
---ペア数列と自然数から展開した自然数を出力する
-tenkai::[(Int,Int)]->Int->[(Int,Int)]
-tenkai [] _ = []
-tenkai s n = reverse $ tenkai' (reverse s) n
+expand::[(Int,Int)]->Int->[(Int,Int)]
+expand [] _ = []
+expand xs n = reverse $ expand' (reverse xs) n
 
---プログラムでは右から左に数列を並べたほうが扱いやすいので反転
---刈子の添字で挙動を変える
-tenkai'::[(Int,Int)]->Int->[(Int,Int)]
-tenkai' ((0,0):s) _ = s
-tenkai' ((x,y):s) n = if y == 0
-    then yokoVan ((x,y):s) n
-    else tateVan ((x,y):s) n
-
---刈子の添字が0なら自分より強い家族ごと横に増やす
-yokoVan::[(Int,Int)]->Int->[(Int,Int)]
-yokoVan ((x,y):s) n = van b 0 n ++ s
+expand'::[(Int,Int)]->Int->[(Int,Int)]
+expand' ((0,0):xs) _ = xs
+expand' (x:xs) n = (vann b d n) ++ xs
     where
-        b = take r s
-        r = root0 ((x,y):s) 0
+        b  = take r xs
+        d = if snd x == 0 then 0 else fst x - fst (xs !! (r - 1))
+        r = root (x:xs) 0
 
---ハムペアにとって最も原始的な親探査
---s1xによって家族の定義が変わる
+vann::[(Int,Int)]->Int->Int->[(Int,Int)]
+vann _ _ 0 = []
+vann b d n = (vann b' d (n-1)) ++ b'
+    where
+        b' = map (\(x,y)->(x+d,y)) b
+
+root::[(Int,Int)]->Int->Int
+root ts x = if snd (ts !! 0) == 0
+                then root0 ts x
+                else root1 ts x
+     
 root0::[(Int,Int)]->Int->Int
-root0 s x = if s1x == 0
-        then fb0 s x
-        else fb1 s x
-    where s1x = snd $ s !! x
-
---s1xが0のときの家族バトル
---家族の定義はfml0
-fb0::[(Int,Int)]->Int->Int
-fb0 s x = if null oji ||fml oya == z 
-    then last (oya:oji)
-    else minimum [t | t <- oya:oji, fml t == z]
-    where 
-        z = minimum [fml t | t <- oji]
-        (oya:oji) = [t | t <- [x..length s -1] , p (p x) == p t]
-        fml = fml0 s
-        p = p0 s
-
---s1xが0でないときの家族バトル
---家族の定義はfml1
-fb1::[(Int,Int)]->Int->Int
-fb1 s x = if null oji ||fml oya == z
-    then last (oya:oji)
-    else minimum [t | t <- oya:oji, fml t == z]
-    where 
-        z = minimum [fml t | t <- oji]
-        (oya:oji) = [t | t <- [x..length s -1] , p (p x) == p t]
-        fml = fml1 s
-        p = p0 s
-
---sxを親に含むノード全部が家族
-fml0::[(Int,Int)]->Int->[(Int,Int)]
-fml0 s x = reverse $ sx:s'
+root0 ts x = last sj
     where
-        (s',_) = break (\y -> fst y <= fst sx) left
-        (sx:left) = reverse $ take (x+1) s
+        (sj,_) = break (\k->fml k < fml (head j)) j
+        j = [k | k<-[1..len] ,p (p x) == p k]
+        fml z =  (ts !! z):(reverse $ fst $ break (< piv z) (reverse (take z ts)))
+        piv z = (fst(ts !! z)+1,snd(ts !! x))
+        p z = if fst (ts !! z) == 0 
+                then -1 
+                else minimum [k | k<-[z..len] , fst (ts !! k) < fst (ts !! z) ]
+        len = length ts - 1
 
---sxを親に含むが、ある地点で区切る
-fml1::[(Int,Int)]->Int->[(Int,Int)]
-fml1 s x = reverse $ sx:s'
+root1::[(Int,Int)]->Int->Int
+root1 ts x = soe !! (root s' 0)
     where
-        (s',_) = break (\y -> (fst y <= fst sx) || (snd y == 0 && fst y == fst sx + 1)) left
-        (sx:left) = reverse $ take (x+1) s
+        s' = zip (map (\k->snd (ts !! k)) soe) (take (ts00 + 1) (repeat 0))
+        soe = take (ts00 + 1) (iterate roots 0)
+        roots = root0 ts
+        ts00 = fst(ts !! 0)
 
---刈子の添字が0でないなら縦に上昇
-tateVan::[(Int,Int)]->Int->[(Int,Int)]
-tateVan ((x,y):s) n = van b d n ++ s
-    where
-        d = x - fst (s !! (r - 1))
-        b = take r s
-        r = root1 ((x,y):s)
-
---root0でノードを辿り、
---そのノードに書いてある整数つまりペアの右の数を
---もとにヒドラを描き、root0でバッドルート探査。
-root1::[(Int,Int)]->Int
-root1 s = tobi !! root0 soe 0
-    where
-        soe = zip ([snd (s !! t) | t<-tobi]) (replicate s00 0)
-        tobi = take s00 (iterate r0 0)
-        s00 = 1 + fst (head s)
-        r0 = root0 s
-
---dだけベアの左の数を加えながら、増殖させる
-van::[(Int,Int)]->Int->Int->[(Int,Int)]
-van _ _ 0 = []
-van s d n = van s' d (n-1) ++ s'
-    where s' = josyo s d
-
---左だけ増やすのを縮めただけ もっとうまく書けそう
-josyo::[(Int,Int)]->Int->[(Int,Int)]
-josyo s d = zip (map (+d) s0) s1
-    where (s0,s1) = unzip s
-
---バシク行列のp0と同じ、単に親
-p0::[(Int,Int)]->Int->Int 
-p0 _ (-1) = -1
-p0 s x = if s0x == 0
-    then -1
-    else minimum [t | t <- [x..length s0 -1], s0 !! t < s0x]
-    where 
-        s0 = map fst s
-        s0x = s0 !! x
+main :: IO ()
+main = print $ expand [(0,0),(1,1),(1,0),(1,0),(2,1)] 3
 
 --展開しつつ角括弧の中を増やす感じ
 keisan::[(Int,Int)]->Int->Int
 keisan [] n = n
-keisan s n = keisan (tenkai s n) 10^n
+keisan s n = keisan (expand s n) 10^n
 
 --ハムペアの限界を召喚する
 hamPair::Int->Int 
-hamPair n = keisan s n
+hamPair n = expand s n
     where s = zip [0..n] [0..n]
 
 --fをたくさん自己合成した関数を返す
